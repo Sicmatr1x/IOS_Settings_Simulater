f529c5e0223a8f303bbceb8ed67737f602a3096e
joe <joe.guo@oocl.com>
2019-03-12 17:39:56 +0800

    add icon imgs and add to settings page

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -37,24 +37,150 @@
 
     <f7-list>
       <f7-list-item v-bind:title="this.$root.language.settings_page.basic_setting_block.airplane_mode">
-        <img slot="media" src="../../assets-src/web-icon.png" style="height: 15px">
+        <img slot="media" src="../../assets-src/systemAppIcons/airplane_mode.png" style="height: 35px">
         <f7-toggle color="green"></f7-toggle>
       </f7-list-item>
       <f7-list-item v-bind:title="this.$root.language.settings_page.basic_setting_block.wifi" link="/wifi/">
-        <img slot="media" src="../../assets-src/web-icon.png" style="height: 15px">
+        <img slot="media" src="../../assets-src/systemAppIcons/wifi.png" style="height: 35px">
       </f7-list-item>
       <f7-list-item v-bind:title="this.$root.language.settings_page.basic_setting_block.Bluetooth" link="/about/">
-        <img slot="media" src="../../assets-src/web-icon.png" style="height: 15px">
+        <img slot="media" src="../../assets-src/systemAppIcons/bluetooth.png" style="height: 35px">
       </f7-list-item>
       <f7-list-item v-bind:title="this.$root.language.settings_page.basic_setting_block.Cellular" link="/about/">
-        <img slot="media" src="../../assets-src/web-icon.png" style="height: 15px">
+        <img slot="media" src="../../assets-src/systemAppIcons/cellular.png" style="height: 35px">
       </f7-list-item>
       <f7-list-item v-bind:title="this.$root.language.settings_page.basic_setting_block.personal_hotspot" link="/about/">
-        <img slot="media" src="../../assets-src/web-icon.png" style="height: 15px">
+        <img slot="media" src="../../assets-src/systemAppIcons/personal_hotspot.png" style="height: 35px">
       </f7-list-item>
       <f7-list-item v-bind:title="this.$root.language.settings_page.basic_setting_block.vpn">
-        <img slot="media" src="../../assets-src/web-icon.png" style="height: 15px">
+        <img slot="media" src="../../assets-src/systemAppIcons/vpn.png" style="height: 35px">
         <f7-toggle color="green"></f7-toggle>
+      </f7-list-item>
+    </f7-list>
+
+    <f7-list>
+      <f7-list-item v-bind:title="this.$root.language.settings_page.basic_setting_block.notifications" link="/about/">
+        <img slot="media" src="../../assets-src/systemAppIcons/notifications.png" style="height: 35px">
+      </f7-list-item>
+      <f7-list-item v-bind:title="this.$root.language.settings_page.basic_setting_block.sounds_haptics" link="/about/">
+        <img slot="media" src="../../assets-src/systemAppIcons/sounds.png" style="height: 35px">
+      </f7-list-item>
+      <f7-list-item v-bind:title="this.$root.language.settings_page.basic_setting_block.do_not_disturb" link="/about/">
+        <img slot="media" src="../../assets-src/systemAppIcons/do_not_disturb.png" style="height: 35px">
+      </f7-list-item>
+      <f7-list-item v-bind:title="this.$root.language.settings_page.basic_setting_block.screen_time" link="/about/">
+        <img slot="media" src="../../assets-src/systemAppIcons/screen_time.png" style="height: 35px">
+      </f7-list-item>
+    </f7-list>
+
+    <f7-list>
+      <f7-list-item v-bind:title="this.$root.language.settings_page.basic_setting_block.general" link="/about/">
+        <img slot="media" src="../../assets-src/systemAppIcons/general.png" style="height: 35px">
+      </f7-list-item>
+      <f7-list-item v-bind:title="this.$root.language.settings_page.basic_setting_block.control_center" link="/about/">
+        <img slot="media" src="../../assets-src/systemAppIcons/control_center.png" style="height: 35px">
+      </f7-list-item>
+      <f7-list-item v-bind:title="this.$root.language.settings_page.basic_setting_block.display_brightness" link="/about/">
+        <img slot="media" src="../../assets-src/systemAppIcons/display_brightness.png" style="height: 35px">
+      </f7-list-item>
+      <f7-list-item v-bind:title="this.$root.language.settings_page.basic_setting_block.wallpaper" link="/about/">
+        <img slot="media" src="../../assets-src/systemAppIcons/wallpaper.png" style="height: 35px">
+      </f7-list-item>
+      <f7-list-item v-bind:title="this.$root.language.settings_page.basic_setting_block.siri_search" link="/about/">
+        <img slot="media" src="../../assets-src/systemAppIcons/siri_search.png" style="height: 35px">
+      </f7-list-item>
+      <f7-list-item v-bind:title="this.$root.language.settings_page.basic_setting_block.touch_id_passcode" link="/about/">
+        <img slot="media" src="../../assets-src/systemAppIcons/touch_id_passcode.png" style="height: 35px">
+      </f7-list-item>
+      <f7-list-item v-bind:title="this.$root.language.settings_page.basic_setting_block.emergency_sos" link="/about/">
+        <img slot="media" src="../../assets-src/systemAppIcons/emergency_sos.png" style="height: 35px">
+      </f7-list-item>
+      <f7-list-item v-bind:title="this.$root.language.settings_page.basic_setting_block.battery" link="/about/">
+        <img slot="media" src="../../assets-src/systemAppIcons/battery.png" style="height: 35px">
+      </f7-list-item>
+      <f7-list-item v-bind:title="this.$root.language.settings_page.basic_setting_block.privacy" link="/about/">
+        <img slot="media" src="../../assets-src/systemAppIcons/privacy.png" style="height: 35px">
+      </f7-list-item>
+    </f7-list>
+
+    <f7-list>
+      <f7-list-item v-bind:title="this.$root.language.settings_page.basic_setting_block.itunes_app_store" link="/about/">
+        <img slot="media" src="../../assets-src/systemAppIcons/iTunes_app_store.png" style="height: 35px">
+      </f7-list-item>
+      <f7-list-item v-bind:title="this.$root.language.settings_page.basic_setting_block.wallet_apple_pay" link="/about/">
+        <img slot="media" src="../../assets-src/systemAppIcons/wallet_apple_pay.png" style="height: 35px">
+      </f7-list-item>
+    </f7-list>
+
+    <f7-list>
+      <f7-list-item v-bind:title="this.$root.language.settings_page.basic_setting_block.password_accounts" link="/about/">
+        <img slot="media" src="../../assets-src/systemAppIcons/passwords_accounts.png" style="height: 35px">
+      </f7-list-item>
+      <f7-list-item v-bind:title="this.$root.language.settings_page.basic_setting_block.mail" link="/about/">
+        <img slot="media" src="../../assets-src/systemAppIcons/mail.png" style="height: 35px">
+      </f7-list-item>
+      <f7-list-item v-bind:title="this.$root.language.settings_page.basic_setting_block.contacts" link="/about/">
+        <img slot="media" src="../../assets-src/systemAppIcons/contacts.png" style="height: 35px">
+      </f7-list-item>
+      <f7-list-item v-bind:title="this.$root.language.settings_page.basic_setting_block.calendar" link="/about/">
+        <img slot="media" src="../../assets-src/systemAppIcons/calendar.png" style="height: 35px">
+      </f7-list-item>
+      <f7-list-item v-bind:title="this.$root.language.settings_page.basic_setting_block.notes" link="/about/">
+        <img slot="media" src="../../assets-src/systemAppIcons/notes.png" style="height: 35px">
+      </f7-list-item>
+      <f7-list-item v-bind:title="this.$root.language.settings_page.basic_setting_block.reminders" link="/about/">
+        <img slot="media" src="../../assets-src/systemAppIcons/reminders.png" style="height: 35px">
+      </f7-list-item>
+      <f7-list-item v-bind:title="this.$root.language.settings_page.basic_setting_block.voice_memos" link="/about/">
+        <img slot="media" src="../../assets-src/systemAppIcons/voice_memos.png" style="height: 35px">
+      </f7-list-item>
+      <f7-list-item v-bind:title="this.$root.language.settings_page.basic_setting_block.phone" link="/about/">
+        <img slot="media" src="../../assets-src/systemAppIcons/phone.png" style="height: 35px">
+      </f7-list-item>
+      <f7-list-item v-bind:title="this.$root.language.settings_page.basic_setting_block.messages" link="/about/">
+        <img slot="media" src="../../assets-src/systemAppIcons/message.png" style="height: 35px">
+      </f7-list-item>
+      <f7-list-item v-bind:title="this.$root.language.settings_page.basic_setting_block.facetime" link="/about/">
+        <img slot="media" src="../../assets-src/systemAppIcons/facetime.png" style="height: 35px">
+      </f7-list-item>
+      <f7-list-item v-bind:title="this.$root.language.settings_page.basic_setting_block.maps" link="/about/">
+        <img slot="media" src="../../assets-src/systemAppIcons/maps.png" style="height: 35px">
+      </f7-list-item>
+      <f7-list-item v-bind:title="this.$root.language.settings_page.basic_setting_block.measure" link="/about/">
+        <img slot="media" src="../../assets-src/systemAppIcons/measure.png" style="height: 35px">
+      </f7-list-item>
+      <f7-list-item v-bind:title="this.$root.language.settings_page.basic_setting_block.safari" link="/about/">
+        <img slot="media" src="../../assets-src/systemAppIcons/safari.png" style="height: 35px">
+      </f7-list-item>
+      <f7-list-item v-bind:title="this.$root.language.settings_page.basic_setting_block.news" link="/about/">
+        <img slot="media" src="../../assets-src/systemAppIcons/news.png" style="height: 35px">
+      </f7-list-item>
+    </f7-list>
+
+    <f7-list>
+      <f7-list-item v-bind:title="this.$root.language.settings_page.basic_setting_block.music" link="/about/">
+        <img slot="media" src="../../assets-src/systemAppIcons/music.png" style="height: 35px">
+      </f7-list-item>
+      <f7-list-item v-bind:title="this.$root.language.settings_page.basic_setting_block.videos" link="/about/">
+        <img slot="media" src="../../assets-src/systemAppIcons/videos.png" style="height: 35px">
+      </f7-list-item>
+      <f7-list-item v-bind:title="this.$root.language.settings_page.basic_setting_block.photos" link="/about/">
+        <img slot="media" src="../../assets-src/systemAppIcons/photos.png" style="height: 35px">
+      </f7-list-item>
+      <f7-list-item v-bind:title="this.$root.language.settings_page.basic_setting_block.camera" link="/about/">
+        <img slot="media" src="../../assets-src/systemAppIcons/camera.png" style="height: 35px">
+      </f7-list-item>
+      <f7-list-item v-bind:title="this.$root.language.settings_page.basic_setting_block.podcasts" link="/about/">
+        <img slot="media" src="../../assets-src/systemAppIcons/podcasts.png" style="height: 35px">
+      </f7-list-item>
+      <f7-list-item v-bind:title="this.$root.language.settings_page.basic_setting_block.game_center" link="/about/">
+        <img slot="media" src="../../assets-src/systemAppIcons/game_center.png" style="height: 35px">
+      </f7-list-item>
+    </f7-list>
+
+    <f7-list>
+      <f7-list-item v-bind:title="this.$root.language.settings_page.basic_setting_block.developer" link="/about/">
+        <img slot="media" src="../../assets-src/systemAppIcons/developer.png" style="height: 35px">
       </f7-list-item>
     </f7-list>
 
